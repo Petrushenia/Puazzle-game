0aa8bad98556508608092e734f877b406a8a868f
Petrushenia <sergworkyourself@gmail.com>
2022-11-28 20:57:42 +0300

    feat: add function for start game

--- src/assets/js/game.js
+++ src/assets/js/game.js
@@ -3,19 +3,24 @@ class Game {
     this.root = root;
     this.game = document.createElement('div');
     this.controlBlock = document.createElement('div');
-    this.controlButtons = ['Shuffle and start', 'Stop', 'Save', 'Results',];
-    this.levelBlocks = document.createElement('div');
-    this.levelButtons = ['3x3', '4x4', '5x5', '6x6', '7x7', '8x8',];
+    this.levelBlock = document.createElement('div');
     this.gameCount = document.createElement('div');
-    this.gameMoves = document.createElement('div');
     this.gameTime = document.createElement('div');
     this.gameField = document.createElement('div');
     this.frameSize = document.createElement('div');
+
+    this.gameMoves = 0;
+    this.controlButtons = ['Shuffle and start', 'Stop', 'Save', 'Results',];
+    this.levelButtons = ['3x3', '4x4', '5x5', '6x6', '7x7', '8x8',];
+    this.emptyCell
+
   }
 
-  start = () => {
+  start = (e) => {
     this.addClassMainBlocks();
     this.createButton();
+    this.createLevelButton();
+    this.addFrameSize(e);
     this.addMainElement();
 
   }
@@ -25,22 +30,32 @@ class Game {
     this.controlBlock.className = 'button-wrapper';
     this.gameCount.className = 'game-count'
     this.gameField.className = 'game-field';
+    this.frameSize.className = 'frame'
+    this.levelBlock.className = 'level-buttons';
   }
 
   addMainElement = () => {
-    this.game.append(this.controlBlock, this.gameCount, this.gameField);
+    this.createCountMove()
+    this.createTime()
+    this.game.append(this.controlBlock, this.gameCount, this.gameField, this.frameSize, this.levelBlock);
     this.root.append(this.game);
   }
 
   createCountMove = () => {
     const move = document.createElement('span');
+    const moveCount = document.createElement('span');
+    moveCount.innerHTML = 0;
     move.className = 'move';
-    move.textContent = `Move: ${0}`;
+    move.innerHTML = 'Moves: ';
+    move.append(moveCount)
+    this.gameCount.appendChild(move);
   }
 
   createTime = () => {
     const time = document.createElement('span');
     time.className = 'time';
+    time.textContent = 'Time:'
+    this.gameCount.appendChild(time);
   }
 
   createButton = () => {
@@ -48,10 +63,49 @@ class Game {
       const btn = document.createElement('button');
       btn.textContent = button;
       if (button == this.controlButtons[1]) {btn.classList.add('stop-button')};
-      btn.classList.add('button-control');
+      btn.classList.add('button');
       this.controlBlock.appendChild(btn);
     })
   }
+
+  addFrameSize = (e) => {
+    if (!e) {
+      this.frameSize.textContent = this.levelButtons[0];
+    }else {
+      this.frameSize.textContent = e.target.textContent
+    }
+  }
+
+  createLevelButton = () => {
+    this.levelButtons.forEach(button => {
+      const btn = document.createElement('button');   
+      btn.textContent = button;
+      btn.className = 'button';
+      btn.addEventListener('click', (e) => {
+          this.showGame()
+          this.setLevel(e)
+          this.addFrameSize(e)
+        }
+      )
+      this.levelBlock.appendChild(btn)
+    })
+  }
+
+  createCell = (level) => {
+    for (let i = 0; i < Math.pow(level, 2); i++) {
+
+    }
+  }
+
+  setLevel = (e) => {
+    return +e.target.textContent[0]
+  }
+
+  showGame = () => {
+    this.gameField.classList.add('show-game')
+  }
+  
+
 } 
 
 const game = new Game(document.body)
